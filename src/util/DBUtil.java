cede88e717d050a394ea4450eb2f776336796333
win10 <win10@DESKTOP-KILUD9T>
2020-01-06 13:31:51 +0800

    增加数据库连接池

--- src/util/DBUtil.java
+++ src/util/DBUtil.java
@@ -1,13 +1,18 @@
 package util;
 
+import java.io.IOException;
 import java.sql.Connection;
 import java.sql.Driver;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.Properties;
 
 import org.apache.jasper.tagplugins.jstl.core.Set;
+
+import com.alibaba.druid.pool.DruidDataSource;
+import com.alibaba.druid.pool.DruidDataSourceFactory;
 
 
 
@@ -15,19 +20,31 @@ public class DBUtil {
 	
 	private DBUtil(){}
 	
+	private static Properties prop=new Properties();
+	private static DruidDataSource dds=null;
+	
 	static {
 		
+//		try {
+//			Class.forName("com.mysql.cj.jdbc.Driver");
+//		} catch (ClassNotFoundException e) {
+//			
+//			e.printStackTrace();
+//		}
+		
 		try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
+			prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("db_server.properties"));
+			dds=(DruidDataSource) DruidDataSourceFactory.createDataSource(prop);
+			
+		} catch (Exception e) {
 			
 			e.printStackTrace();
 		}
 	}
 	
-	private static final String url="jdbc:mysql://localhost:3306/training?serverTimezone=UTC&characterEncoding=UTF-8";
-	private static final String user="root";
-	private static final String password="root";
+//	private static final String url="jdbc:mysql://localhost:3306/training?serverTimezone=UTC&characterEncoding=UTF-8";
+//	private static final String user="root";
+//	private static final String password="root";
 	
 	private static  ThreadLocal<Connection> t=new ThreadLocal<Connection>();
 	
@@ -36,7 +53,8 @@ public class DBUtil {
 		
 		Connection conn =t.get(); 
 		if (conn==null) {
-			conn=DriverManager.getConnection(url, user, password);
+			//conn=DriverManager.getConnection(url, user, password);
+			conn=dds.getConnection();
 			t.set(conn);
 		}
 		return conn;
